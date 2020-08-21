a349d05d3a1ecc1b66b610201a2fb62b2056427c
Oscar.Quesada <quesadaao@hotmail.com>
2020-08-20 20:40:03 -0600

    Word Finder

---
+++ ConsoleTwoDimensional/ConsoleTwoDimensional/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTwoDimensional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] array3Db = new char[3, 3] { { 'x', 'y','z' }, 
+                                                { 'd', 'o','g' },
+                                                { 'a', 'b','c' } 
+                                              };
+            var res = exist("dog", array3Db);
+            Console.WriteLine("dog, exist in the matrix: "+ res);
+        }
+
+        private static bool exist(string word, char[,] matrix) {
+            string buildword = string.Empty;
+
+            foreach (char t in matrix)
+            {
+                buildword = buildword + t;
+                if (buildword.Length == word.Length)
+                {
+                    if (buildword.Equals(word))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        buildword = string.Empty;
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int z = 0; z < matrix.GetLength(0); z++)
+                {
+                    buildword = buildword + matrix[z, i];
+                    if (buildword.Length == word.Length)
+                    {
+                        if (buildword.Equals(word))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            buildword = string.Empty;
+                        }
+                    }
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
